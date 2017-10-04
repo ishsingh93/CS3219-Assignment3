63e812c57c02d0fb96664a68844d33b02926c367
ishsingh93 <ishsingh93@gmail.com>
2017-10-04 14:15:26 +0800

    Added attributes to Input class and input/output methods to UI class

---
+++ src/main/java/assignment3_CIR/app/Input.java
@@ -0,0 +1,14 @@
+package assignment3_CIR.app;
+
+import java.util.ArrayList;
+
+public class Input {
+	
+	private static String queryType;
+	private static ArrayList<String> datasets;
+	private static ArrayList<Integer> numYrs;
+	private static ArrayList<String> conferences;
+	private static ArrayList<String> authors;
+	
+
+}
---
+++ src/main/java/assignment3_CIR/app/UI.java
@@ -0,0 +1,21 @@
+package assignment3_CIR.app;
+
+import java.util.Scanner;
+
+public class UI {
+
+	private Scanner sc = new Scanner(System.in);
+	
+	public UI() {
+		
+	}
+	
+	public String getInput() {
+		String input = sc.nextLine();
+		return input;
+	}
+	
+	public Output displayOutput() {
+		return null;
+	}
+}
