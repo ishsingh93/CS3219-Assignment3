bfb939e9175fde2a2a6223dfaae2486e45eda56d
Javan <javanhuangzx@gmail.com>
2017-10-07 14:02:56 +0800

    DataManager updates

--- src/main/java/assignment3_CIR/app/DataManager.java
+++ src/main/java/assignment3_CIR/app/DataManager.java
@@ -1,5 +1,59 @@
 package assignment3_CIR.app;
 
+import java.io.File;
+import java.util.List;
+
+import org.json.JSONObject;
+import assignment3_CIR.app.DataObj;
+
 public class DataManager {
 
+	private static final DataObj DataObj = null;
+
+	public DataManager() {
+
+	}
+	
+	public static int countCitations(JSONObject testObj) {
+		int lengthOfObj = 0;
+		JSONObject ob1 = testObj.getJSONObject("algorithms");
+		org.json.JSONArray ar2 = ob1.getJSONArray("algorithm");
+		JSONObject ob3 = (JSONObject) ar2.get(2);
+		JSONObject ob4 = ob3.getJSONObject("citationList");
+		org.json.JSONArray ob5 = ob4.getJSONArray("citation");
+		for (int i=0; i < ob5.length(); i++) {
+			//String validString = ob5.getJSONObject(i).has("valid");
+			if (ob5.getJSONObject(i).has("valid")) {
+				lengthOfObj++;
+			}
+		}
+		return lengthOfObj;
+	}
+	
+	public static DataObj listCitedDocumentsByYears(String conference, File homeDirectory, List<Integer> rangeYears) {
+		return DataObj;
+	}
+	
+	public static int citedDocuments(JSONObject testObj, int year) {
+		int citedDocuments = 0;
+		String date;
+		JSONObject ob1 = testObj.getJSONObject("algorithms");
+		org.json.JSONArray ar2 = ob1.getJSONArray("algorithm");
+		JSONObject ob3 = (JSONObject) ar2.get(2);
+		JSONObject ob4 = ob3.getJSONObject("citationList");
+		org.json.JSONArray ar5 = ob4.getJSONArray("citation");
+		System.out.println(ar5.toString());
+			for (int i=0; i < ar5.length(); i++) {
+				if (ar5.getJSONObject(i).has("valid")) {
+					JSONObject ob6 = (JSONObject) ar5.getJSONObject(2);
+					date = ob6.getString("date");
+					System.out.println(date);
+					int datedYear = Integer.parseInt(date);
+				//if (datedYear == year) {
+				//	citedDocuments++;
+				//}
+			}
+		}
+		return citedDocuments;
+	}
 }
