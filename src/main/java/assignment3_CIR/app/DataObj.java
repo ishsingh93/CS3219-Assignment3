02b7eb9d93c9e5e437ef2fb6f228856ec8fe7604
Javan <javanhuangzx@gmail.com>
2017-10-11 11:48:46 +0800

    Getters and Setters for the following objects

--- src/main/java/assignment3_CIR/app/DataObj.java
+++ src/main/java/assignment3_CIR/app/DataObj.java
@@ -1,5 +1,39 @@
 package assignment3_CIR.app;
 
+import java.util.List;
+
 public class DataObj {
 
+	private List<Integer> noOfCitedDocuments;
+	private List<Integer> rangeOfYears;
+	private List<String> citedConferences;
+	public DataObj() {
+		
+	}
+	
+	//Getters
+	public List<Integer> getNoOfCitedDocuments() {
+		return noOfCitedDocuments;
+	}
+	
+	public List<String> getCitedConferences() {
+		return citedConferences;
+	}
+	
+	public List<Integer> getRangeOfYears() {
+		return rangeOfYears;
+	}
+	
+	public void setNoOfCitedDocuments(List<Integer> citedDocs) {
+		this.noOfCitedDocuments = citedDocs;
+	}
+	
+	public void setRangeOfYears(List<Integer> yearRange) {
+		this.rangeOfYears = yearRange;
+	}
+	
+	public void setCitedConferences(List<String> citedConf) {
+		this.citedConferences = citedConf;
+	}
+		
 }
--- src/main/java/assignment3_CIR/app/Input.java
+++ src/main/java/assignment3_CIR/app/Input.java
@@ -1,14 +1,78 @@
 package assignment3_CIR.app;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Input {
 	
-	private static String queryType;
-	private static ArrayList<String> datasets;
-	private static ArrayList<Integer> numYrs;
-	private static ArrayList<String> conferences;
-	private static ArrayList<String> authors;
+	private String homeDirectory;
+	private String queryType;
+	private String queryInstruction;
+	private ArrayList<String> datasets;
+	private List<Integer> numYrs;
+	private ArrayList<String> conferences;
+	private ArrayList<String> authors;
 	
-
+	//Constructors
+	public Input() {
+		
+	}
+	
+	//Getters
+	public String getHomeDirectory() {
+		return homeDirectory;
+	}
+	
+	public String getQueryType() {
+		return queryType;
+	}
+	
+	public String getQueryInstruction() {
+		return queryInstruction;
+	}
+	
+	public ArrayList<String> getDatasets() {
+		return datasets;
+	}
+	
+	public List<Integer> getNumYrs() {
+		return numYrs;
+	}
+	
+	public ArrayList<String> getConferences() {
+		return conferences;
+	}
+	
+	public ArrayList<String> getAuthors() {
+		return authors;
+	}
+	
+	//Setters
+	public void setHomeDirectory(String homeDir) {
+		this.homeDirectory = homeDir;
+	}
+	
+	public void setQueryType(String query) {
+		this.queryType = query;
+	}
+	
+	public void setQueryInstruction(String queryInstr) {
+		this.queryInstruction = queryInstr;
+	}
+	
+	public void setDatasets(ArrayList<String> listOfDatasets) {
+		this.datasets = listOfDatasets;
+	}
+	
+	public void setNumYrs(List<Integer> listOfYears) {
+		this.numYrs = listOfYears;
+	}
+	
+	public void setConferences(ArrayList<String> listOfConferences) {
+		this.conferences = listOfConferences;
+	}
+	
+	public void setAuthors(ArrayList<String> listOfAuthors) {
+		this.authors = listOfAuthors;
+	}
 }
